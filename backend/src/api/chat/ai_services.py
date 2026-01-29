2fbb0d46e5430eef4046fc392e031bf670311c6b
sahilsinhacodes <sinhasahil423@gmail.com>
2026-01-29 21:55:44 +0530

    Update ai_services.py

--- backend/src/api/chat/ai_services.py
+++ backend/src/api/chat/ai_services.py
@@ -1,6 +1,14 @@
 import os
 
+from pydantic import BaseModel, Field
+
 from langchain_openai import ChatOpenAI
+
+
+class EmailMessage(BaseModel):
+    subject: str
+    contents: str
+    invalid_request: bool | None = Field(default=False)
 
 
 OPENAI_BASE_URL = os.environ.get('OPENAI_BASE_URL') or None
@@ -18,3 +26,18 @@ if OPENAI_BASE_URL:
     openai_params['base_url'] = OPENAI_BASE_URL
 
 llm_base = ChatOpenAI(**openai_params)
+
+llm = llm_base.with_structured_output(EmailMessage)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant for research and composing plaintext emails. Do not use markdown in your response only plaintext.",
+    ),
+    ("human", "Create an email about the benefits of coffee. Do not use markdown in your response only plaintext"),
+]
+
+
+response = llm.invoke(messages)
+
+print(response)
