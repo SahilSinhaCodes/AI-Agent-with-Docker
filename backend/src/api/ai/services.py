3961e6a0c8ba094e6268ae739f5ffbc91e173111
sahilsinhacodes <sinhasahil423@gmail.com>
2026-01-29 22:58:08 +0530

    Update services.py

--- backend/src/api/ai/services.py
+++ backend/src/api/ai/services.py
@@ -5,7 +5,7 @@ from api.ai.schemas import EmailMessageSchema
 
 def generate_email_message(query:str) -> EmailMessageSchema:
     llm_base = get_openai_llm()
-    llm = llm_base.with_structured_output(EmailMessageSchema)
+    llm = llm_base.with_structured_output(EmailMessageSchema, method="json_mode")
 
     messages = [
         (
