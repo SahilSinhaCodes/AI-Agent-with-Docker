5b9d102e82dff731093c00d7c7df2e690a190f27
sahilsinhacodes <sinhasahil423@gmail.com>
2026-02-01 16:41:13 +0530

    LangGraph Supervisor

--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -1,28 +1,40 @@
-from fastapi import APIRouter, Depends
-from sqlmodel import Session,select
 from typing import List
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+
 from api.db import get_session
-from api.ai.schemas import EmailMessageSchema
+from api.ai.agents import get_supervisor
+from api.ai.schemas import EmailMessageSchema, SupervisorMessageSchema
 from api.ai.services import generate_email_message
 from .models import ChatMessagePayload, ChatMessage, ChatMessageListItem
-
 router = APIRouter()
 
 # /api/chats/
-@router.get("")
+@router.get("/")
 def chat_health():
     return {"status": "ok"}
 
-@router.get("/recent/",response_model=List[ChatMessageListItem])
+# /api/chats/recent/
+# curl http://localhost:8080/api/chats/recent/
+@router.get("/recent/", response_model=List[ChatMessageListItem])
 def chat_list_messages(session: Session = Depends(get_session)):
     query = select(ChatMessage) # sql -> query
     results = session.exec(query).fetchall()[:10]
     return results
 
-# HTTP POST -> payload = {"message": "Hello world"} -> {"message": "hello world", "id": 1}
-# curl -X POST -d '{"message": "Give me a summary of why it's good to go outside"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
 
-@router.post("", response_model=EmailMessageSchema)
+# HTTP POST -> payload = {"message": "Hello world"} -> {"message": "hello world", "id": 1}
+# curl -X POST -d '{"message": "Hello world"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
+# curl -X POST -d '{"message": "Hello world"}' -H "Content-Type: application/json"  https://docker-api-fastapi-python-app-3jid9.ondigitalocean.app/api/chats/
+
+# curl -X POST -d '{"message": "Give me a summary of why it is good to go outside"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
+
+# curl -X POST -d '{"message": "Give me a summary of why it is good to go outside"}' -H "Content-Type: application/json" https://docker-api-fastapi-python-app-3jid9.ondigitalocean.app/api/chats/
+
+
+# curl -X POST -d '{"message": "Research why it is good to go outside and email me the results"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
+
+@router.post("/", response_model=SupervisorMessageSchema)
 def chat_create_message(
         payload:ChatMessagePayload,
         session: Session = Depends(get_session)
@@ -31,7 +43,18 @@ def chat_create_message(
     obj = ChatMessage.model_validate(data)
     session.add(obj)
     session.commit()
-    # session.refresh(obj) # ensure id/primary key added to the obj instance
-    # ready to store in the database
-    response = generate_email_message(payload.message)
-    return response
+    supe = get_supervisor()
+    msg_data = {
+        "messages": [
+            {"role": "user",
+             "content": f"{payload.message}"
+             },
+        ]
+    }
+    result = supe.invoke(msg_data)
+    if not result:
+        raise HTTPException(status_code=400, detail="Error with supervisor")
+    messages = result.get("messages")
+    if not messages:
+        raise HTTPException(status_code=400, detail="Error with supervisor")
+    return messages[-1]
