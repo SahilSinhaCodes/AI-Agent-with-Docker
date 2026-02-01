e66fd54508b32e614fa58ff6865ff229e74d67e7
sahilsinhacodes <sinhasahil423@gmail.com>
2026-02-01 15:13:02 +0530

    Update agents.py

--- backend/src/api/ai/agents.py
+++ backend/src/api/ai/agents.py
@@ -3,7 +3,8 @@ from api.ai.llms import get_openai_llm
 
 from api.ai.tools import (
     send_me_email,
-    get_unread_emails
+    get_unread_emails,
+    research_email
 )
 
 EMAIL_TOOLS_LIST = [
@@ -18,6 +19,17 @@ def get_email_agent():
         model=model,
         tools=EMAIL_TOOLS_LIST,
         prompt="You are a helpful assistant for managing my email inbox for generating, sending, and reviewing emails."
+    )
+
+    return agent
+
+def get_research_agent():
+    model = get_openai_llm()
+    agent = create_react_agent(
+        model=model,
+        tools=[research_email],
+        prompt="You are a helpful research assistant for preparing email data",
+        name='research_agent'
     )
 
     return agent
